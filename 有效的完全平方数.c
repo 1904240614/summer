44c6484f38dc85fc1067168bd87f641ec61a9f89
陈润民 <67950230+1904240614@users.noreply.github.com>
2020-08-05 11:31:39 +0800

    Add files via upload

---
+++ 有效的完全平方数.c
@@ -0,0 +1,38 @@
+/*367. 有效的完全平方数
+给定一个正整数 num，编写一个函数，如果 num 是一个完全平方数，则返回 True，否则返回 False。
+
+说明：不要使用任何内置的库函数，如  sqrt。
+
+示例 1：
+
+输入：16
+输出：True
+示例 2：
+
+输入：14
+输出：False*/
+#include<stdio.h>
+bool isPerfectSquare(int num)
+{
+    if(num<2) return true;
+    long x = num/2;
+    while(x*x>num)
+    {
+        x = (x+num/x)/2;
+    }
+    return x*x == num;
+}
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	if(isPerfectSquare(num))
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
